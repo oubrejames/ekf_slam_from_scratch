78626673a82736cb7123649e27dfd1cb0d7d12d6
James Oubre <oubrejames@gmail.com>
2023-01-11 18:00:08 -0600

    Deleted executable from git

--- turtlelib/rigid2d.cpp
+++ turtlelib/rigid2d.cpp
@@ -4,96 +4,83 @@
 #include <iostream>
 
 
-int main() {
-    // Variable declarations
-    double x=1.2;
-    double y=4.5;
-
-    // Program code
-    int i = turtlelib::almost_equal(x,y);
-    printf("%d\n", i);
-    printf("Deg to rads: %f\n",turtlelib::deg2rad(90.0));
-    printf("Rad to deg: %f\n",turtlelib::rad2deg(3.14));
-
-    return 0;
+/// \brief output a 2 dimensional vector as [xcomponent ycomponent]
+/// os - stream to output to
+/// v - the vector to print
+std::ostream & turtlelib::operator<<(std::ostream & os, const turtlelib::Vector2D & v){
+    os << "[" << v.x << " " << v.y << "]";
+    /// \TODO: Why do we return OS?
+    return os; 
 }
 
-// /// \brief output a 2 dimensional vector as [xcomponent ycomponent]
-// /// os - stream to output to
-// /// v - the vector to print
-// std::ostream & operator<<(std::ostream & os, const Vector2D & v);
+/// \brief input a 2 dimensional vector
+///   You should be able to read vectors entered as follows:
+///   [x y] or x y
+/// \param is - stream from which to read
+/// \param v [out] - output vector
+std::istream & turtlelib::operator>>(std::istream & is, turtlelib::Vector2D & v){
+    if (is.peek() == '['){ // If the first character input is a bracket
+        is.get();               // Remove bracket
+        is >> v.x;              // Store first component in v.x
+        is.get();               // Remove space
+        is >> v.y;              // Store seecond component in v.y
+    }
+    else{
+        is >> v.x >> v.y;
+    }
+    return is;
+}
 
-// /// \brief input a 2 dimensional vector
-// ///   You should be able to read vectors entered as follows:
-// ///   [x y] or x y
-// /// \param is - stream from which to read
-// /// \param v [out] - output vector
-// ///
-// /// The way input works is (more or less): what the user types is stored in a buffer until the user types
-// /// a newline (by pressing enter).  The iostream methods then process the data in this buffer character by character.
-// /// Typically, each character is examined and then removed from the buffer automatically.
-// /// If the characters don't match what is expected (e.g., we are expecting an int but the letter 'q' is encountered)
-// /// an error flag is set on the stream object (e.g., std::cin) and processing stops.
-// ///
-// /// We have lower level control however.
-// /// std::peek() looks at the next unprocessed character in the buffer without removing it
-// ///     https://en.cppreference.com/w/cpp/io/basic_istream/peek
-// /// std::get() removes the next unprocessed character from the buffer.
-// ///     https://en.cppreference.com/w/cpp/io/basic_istream/get
-// /// When you call std::peek() it will wait for there to be at least one character in the buffer (e.g., the user types a character)
-// /// HINT: this function can be written in under 20 lines and uses only std::peek(), std::get(), istream::operator>>() and a little logic
-// std::istream & operator>>(std::istream & is, Vector2D & v);
+/// \brief a rigid body transformation in 2 dimensions
+class Transform2D
+{
+public:
+    /// \brief Create an identity transformation
+    Transform2D();
 
-// /// \brief a rigid body transformation in 2 dimensions
-// class Transform2D
-// {
-// public:
-//     /// \brief Create an identity transformation
-//     Transform2D();
+    /// \brief create a transformation that is a pure translation
+    /// \param trans - the vector by which to translate
+    explicit Transform2D(Vector2D trans);
 
-//     /// \brief create a transformation that is a pure translation
-//     /// \param trans - the vector by which to translate
-//     explicit Transform2D(Vector2D trans);
+    /// \brief create a pure rotation
+    /// \param radians - angle of the rotation, in radians
+    explicit Transform2D(double radians);
 
-//     /// \brief create a pure rotation
-//     /// \param radians - angle of the rotation, in radians
-//     explicit Transform2D(double radians);
+    /// \brief Create a transformation with a translational and rotational
+    /// component
+    /// \param trans - the translation
+    /// \param rot - the rotation, in radians
+    Transform2D(Vector2D trans, double radians);
 
-//     /// \brief Create a transformation with a translational and rotational
-//     /// component
-//     /// \param trans - the translation
-//     /// \param rot - the rotation, in radians
-//     Transform2D(Vector2D trans, double radians);
-
-//     /// \brief apply a transformation to a Vector2D
-//     /// \param v - the vector to transform
-//     /// \return a vector in the new coordinate system
-//     Vector2D operator()(Vector2D v) const;
+    /// \brief apply a transformation to a Vector2D
+    /// \param v - the vector to transform
+    /// \return a vector in the new coordinate system
+    Vector2D operator()(Vector2D v) const;
 
 
-//     /// \brief invert the transformation
-//     /// \return the inverse transformation. 
-//     Transform2D inv() const;
+    /// \brief invert the transformation
+    /// \return the inverse transformation. 
+    Transform2D inv() const;
 
-//     /// \brief compose this transform with another and store the result 
-//     /// in this object
-//     /// \param rhs - the first transform to apply
-//     /// \return a reference to the newly transformed operator
-//     Transform2D & operator*=(const Transform2D & rhs);
+    /// \brief compose this transform with another and store the result 
+    /// in this object
+    /// \param rhs - the first transform to apply
+    /// \return a reference to the newly transformed operator
+    Transform2D & operator*=(const Transform2D & rhs);
 
-//     /// \brief the translational component of the transform
-//     /// \return the x,y translation
-//     Vector2D translation() const;
+    /// \brief the translational component of the transform
+    /// \return the x,y translation
+    Vector2D translation() const;
 
-//     /// \brief get the angular displacement of the transform
-//     /// \return the angular displacement, in radians
-//     double rotation() const;
+    /// \brief get the angular displacement of the transform
+    /// \return the angular displacement, in radians
+    double rotation() const;
 
-//     /// \brief \see operator<<(...) (declared outside this class)
-//     /// for a description
-//     friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+    /// \brief \see operator<<(...) (declared outside this class)
+    /// for a description
+    friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
 
-// };
+};
 
 
 // /// \brief should print a human readable version of the transform:
@@ -116,3 +103,22 @@ int main() {
 // /// \return the composition of the two transforms
 // /// HINT: This function should be implemented in terms of *=
 // Transform2D operator*(Transform2D lhs, const Transform2D & rhs);
+
+int main() {
+    // Variable declarations
+    double x=1.2;
+    double y=4.5;
+
+    // Program code
+    int i = turtlelib::almost_equal(x,y);
+    printf("%d\n", i);
+    printf("Deg to rads: %f\n",turtlelib::deg2rad(90.0));
+    printf("Rad to deg: %f\n",turtlelib::rad2deg(3.14));
+
+    turtlelib::Vector2D c1;
+    std::cout << "Enter a Vector2D: ";
+    std::cin >> c1;
+    std::cout << "x = " << c1.x << " y=" << c1.y << std::endl;
+
+    return 0;
+}
