5386fd711a9d0c0b003a7684f98c956a0ac56c87
James Oubre <oubrejames@gmail.com>
2023-01-21 15:46:22 -0600

    Added 1 marker

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -12,6 +12,7 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "nusim/srv/teleport.hpp"
+#include <visualization_msgs/msg/marker.hpp>
 
 using namespace std::chrono_literals;
 
@@ -19,10 +20,10 @@ using namespace std::chrono_literals;
 * member function as a callback from the timer. */
 
 
-class MinimalPublisher : public rclcpp::Node 
+class NusimNode : public rclcpp::Node 
 {
   public:
-    MinimalPublisher()
+    NusimNode()
     : Node("nusim"), count_(0)
     {
       time_publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
@@ -33,18 +34,18 @@ class MinimalPublisher : public rclcpp::Node
       int hz = this->get_parameter("Hz").get_parameter_value().get<double>();
       auto hz_in_ms = std::chrono::milliseconds((long)(1000/(hz)));
       timer_ = this->create_wall_timer(
-      hz_in_ms, std::bind(&MinimalPublisher::timer_callback, this));
+      hz_in_ms, std::bind(&NusimNode::timer_callback, this));
 
       reset_server_ = this->create_service<std_srvs::srv::Empty>(
         "~/reset",
-        std::bind(&MinimalPublisher::reset, this, std::placeholders::_1, std::placeholders::_2));
+        std::bind(&NusimNode::reset, this, std::placeholders::_1, std::placeholders::_2));
 
       tf_broadcaster_ =
         std::make_unique<tf2_ros::TransformBroadcaster>(*this);
 
       teleport_server_ = this->create_service<nusim::srv::Teleport>(
         "~/teleport",
-        std::bind(&MinimalPublisher::teleport, this, std::placeholders::_1, std::placeholders::_2));
+        std::bind(&NusimNode::teleport, this, std::placeholders::_1, std::placeholders::_2));
 
       auto x0_desc = rcl_interfaces::msg::ParameterDescriptor{};
       x0_desc.description = "Initial x position of turtlebot";
@@ -63,6 +64,31 @@ class MinimalPublisher : public rclcpp::Node
       this->declare_parameter("theta0", 0.0, theta0_desc);
       turtle_theta0 = this->get_parameter("theta0").get_parameter_value().get<double>();
       turtle_theta=turtle_theta0;
+
+      // Marker cylinder stuff
+      marker_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("visualization_marker", 10);
+
+      marker.header.frame_id = "nusim/world";
+      marker.header.stamp = this->get_clock()->now();
+      marker.ns = "my_namespace";
+      marker.id = 0;
+      marker.type = visualization_msgs::msg::Marker::SPHERE;
+      marker.action = visualization_msgs::msg::Marker::ADD;
+      marker.pose.position.x = 1.0;
+      marker.pose.position.y = 1.0;
+      marker.pose.position.z = 1.0;
+      marker.pose.orientation.x = 0.0;
+      marker.pose.orientation.y = 0.0;
+      marker.pose.orientation.z = 0.0;
+      marker.pose.orientation.w = 1.0;
+      marker.scale.x = 1.0;
+      marker.scale.y = 1.0;
+      marker.scale.z = 0.1;
+      marker.color.a = 1.0; // Don't forget to set the alpha!
+      marker.color.r = 0.0;
+      marker.color.g = 1.0;
+      marker.color.b = 0.0;
+
     }
 
   private:
@@ -113,6 +139,8 @@ class MinimalPublisher : public rclcpp::Node
 
       // Send the transformation
       tf_broadcaster_->sendTransform(t);
+      marker_publisher_->publish( marker );
+
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
@@ -120,13 +148,15 @@ class MinimalPublisher : public rclcpp::Node
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_server_;
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
     rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_server_;
+    visualization_msgs::msg::Marker marker;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher_;
     size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<NusimNode>());
   rclcpp::shutdown();
   return 0;
 }
