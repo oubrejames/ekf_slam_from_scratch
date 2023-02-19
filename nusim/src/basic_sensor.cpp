623e681bcca7db9e3eca6495cea95ff1dc968fa1
James Oubre <oubrejames@gmail.com>
2023-02-19 00:18:41 -0600

    Added basic sensor. WOrks somewhat, weird bug with detecting obstacles at right time

---
+++ nusim/src/basic_sensor.cpp
@@ -0,0 +1,155 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/u_int64.hpp"
+#include <std_srvs/srv/empty.hpp>
+#include "tf2_ros/transform_broadcaster.h"
+#include "turtlesim/msg/pose.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "nusim/srv/teleport.hpp"
+
+#include "nuturtlebot_msgs/msg/wheel_commands.hpp"
+#include "turtlelib/diff_drive.hpp"
+#include "nuturtlebot_msgs/msg/sensor_data.hpp"
+#include <vector>
+#include <nav_msgs/msg/path.hpp>
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include <random>
+
+#include "tf2_ros/transform_listener.h"
+#include <visualization_msgs/msg/marker.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
+#include "tf2_ros/buffer.h"
+#include <cmath>
+#include <geometry_msgs/msg/twist.hpp>
+
+using namespace std::chrono_literals;
+
+class BasicSensor : public rclcpp::Node
+{
+public:
+    BasicSensor()
+    : Node("basic_sensor"), count_(0)
+    {
+        // Define parameter to change basic sensor noise
+        auto basic_sensor_variance_desc = rcl_interfaces::msg::ParameterDescriptor();
+        basic_sensor_variance_desc.description = "Variance to change sensor noise for basic sensor";
+        declare_parameter("basic_sensor_variance", 0.01, basic_sensor_variance_desc);
+        basic_sensor_variance = get_parameter("basic_sensor_variance").get_parameter_value().get<double>();
+
+        // Define parameter to change maximum range of the sensor
+        auto max_range_desc = rcl_interfaces::msg::ParameterDescriptor();
+        max_range_desc.description = "Maximum range for basic sensor (m)";
+        declare_parameter("max_range", 1.0, max_range_desc);
+        max_range = get_parameter("max_range").get_parameter_value().get<double>();
+
+        // Create 5 Hz timer
+        auto hz_in_ms = std::chrono::milliseconds((long)(1000 / (5.0)));
+        timer_ = create_wall_timer(
+        hz_in_ms, std::bind(&BasicSensor::timer_callback, this));
+
+        // Create listener to get red robot location
+        tf_buffer_ =
+            std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ =
+            std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+        // Create subcriber to get actual obstacle positions
+         real_obstacles_sub_ = this->create_subscription<visualization_msgs::msg::MarkerArray>(
+            "/nusim/obstacles", 10, std::bind(&BasicSensor::real_obstacles_cb, this, std::placeholders::_1));
+
+        // Publisher to publish sensed obstacles
+        sensed_obstacles_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("~/marker_array", 10);
+    }
+
+private:
+
+    // Timer callback going at 5 hz
+    void timer_callback()
+    {
+        // Check position of the robot 
+        // Look up for the transformation between nusim/world and the red robot frames
+        geometry_msgs::msg::TransformStamped t;
+        try {
+          t = tf_buffer_->lookupTransform(
+            "red/base_footprint", "nusim/world",
+            tf2::TimePointZero);
+        } catch (const tf2::TransformException & ex) {
+          RCLCPP_INFO(
+            this->get_logger(), "Could not transform red/base_footprint to nusim/world");
+          return;
+        }
+        
+        // Get a Gaussian distributin of noise
+        std::normal_distribution<> gaus_dist(0.0, basic_sensor_variance);
+
+        // Loop through each of the sensed obstacles in the marker array
+        for(int i=0; i < (int)sensed_obstacles.markers.size(); i++){
+            // Check if the obstacle intersects with the max range of the robot
+
+            // // Add noise to the position of the obstacle
+            // sensed_obstacles.markers.at(i).pose.position.x += gaus_dist(get_random());
+            // sensed_obstacles.markers.at(i).pose.position.y += gaus_dist(get_random());
+
+            // Caclulate the straight line distance between center of robot to center of obstacle
+            double dist_btw_centers = std::sqrt(
+                (t.transform.translation.x-sensed_obstacles.markers.at(i).pose.position.x)*(t.transform.translation.x-sensed_obstacles.markers.at(i).pose.position.x)
+                + (t.transform.translation.y-sensed_obstacles.markers.at(i).pose.position.y)*(t.transform.translation.y-sensed_obstacles.markers.at(i).pose.position.y));
+
+            // If the straight line distance minus the obsacle radius is less than the max radius of
+            // the robot than the obstacle is in range -> ADD
+            if(dist_btw_centers < max_range){
+                sensed_obstacles.markers.at(i).action = visualization_msgs::msg::Marker::ADD;
+            }
+            //Else -> DELETE
+            else{sensed_obstacles.markers.at(i).action = visualization_msgs::msg::Marker::DELETE;}
+
+            // Change color to yellow
+            sensed_obstacles.markers.at(i).color.r = 0.8;
+            sensed_obstacles.markers.at(i).color.g = 0.8;
+            sensed_obstacles.markers.at(i).color.b = 0.3;
+        }
+
+        // Publish marker array
+        sensed_obstacles_pub_->publish(sensed_obstacles);
+    }
+
+    /// @brief SAY THAT I GOT THIS FROM MATT'S NOTES
+    /// @return 
+    std::mt19937 & get_random()
+    {
+        // static variables inside a function are created once and persist for the remainder of the program
+        static std::random_device rd{}; 
+        static std::mt19937 mt{rd()};
+        // we return a reference to the pseudo-random number genrator object. This is always the
+        // same object every time get_random is called
+        return mt;
+    }
+
+    // Subscribe to get the actual marker positions
+    void real_obstacles_cb(const visualization_msgs::msg::MarkerArray & msg){
+        sensed_obstacles = msg;
+    }
+
+    size_t count_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr real_obstacles_sub_;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr sensed_obstacles_pub_;
+    visualization_msgs::msg::MarkerArray sensed_obstacles;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    double basic_sensor_variance = 0.0, max_range = 0.0;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<BasicSensor>());
+    rclcpp::shutdown();
+    return 0;
+}
